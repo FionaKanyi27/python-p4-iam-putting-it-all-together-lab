b016a618abdc2791d4b5522c6d4e856eb5f2f95b
Fiona <fionakanyi0@gmail.com>
2025-06-07 01:26:09 +0300

    commit first

--- server/models.py
+++ server/models.py
@@ -7,7 +7,39 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+        id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+    
+    # Relationship
+    recipes = db.relationship('Recipe', backref='user', cascade='all, delete-orphan')
+    
+    # Serialization rules
+    serialize_rules = ('-_password_hash', '-recipes.user')
+    
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError("Username must be present")
+        return username
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
